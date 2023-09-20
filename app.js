43399baecf302792633241cd66969039300f6ab2
Joujoniki Asa Roy <139947026+JoujonikiAsa2@users.noreply.github.com>
2023-09-20 10:43:05 +0600

    Update app.js updated

--- app.js
+++ app.js
@@ -1,145 +1,129 @@
-const handleCategories = async (isShowAll) => {
-  const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
+const ShowBtn = document.getElementById("show-all");
+let categoryIdToShow = null;
+let isShowingAll = false;
+let sortFlag = false;
+const sortBtn = document.querySelector("#sortBtn");
+
+sortBtn.addEventListener("click", function () {
+  sortFlag = true;
+  fetchAndDisplayNews();
+  console.log(sortFlag);
+});
+const handleCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories"
+  );
   const data = await res.json();
   const news_category = data.data.news_category;
 
-  // connect with tab-container div
-  if (!isShowAll) {
-    const tabContainer = document.getElementById('tab-container')
-    news_category.forEach((category) => {
-      console.log(category, isShowAll)
-      const categoryContainer = document.createElement('div');
-      categoryContainer.innerHTML = `
-            <a onclick="handleCategoryNews('${category.category_id}',${isShowAll})" class="tab">${category.category_name}</a>`;
-      tabContainer.appendChild(categoryContainer);
-      console.log(categoryContainer)
-      console.log(`'${category.category_id}'`)
-    });
-  }
-  else {
-    news_category.forEach((category) => {
-      console.log(category, isShowAll)
-      handleCategoryNews(`'${category.category_id}'`, isShowAll)
-      console.log(`'${category.category_id}'`)
-    });
-  }
+  const tabContainer = document.getElementById("tab-container");
+  tabContainer.innerHTML = "";
+
+  news_category.forEach((category) => {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.innerHTML = `
+        <a onclick="handleCategoryNews('${category.category_id}')" class="tab">${category.category_name}</a>`;
+    tabContainer.appendChild(categoryContainer);
+  });
+
+  handleCategoryNews(news_category[0].category_id); // Load the first category initially
 };
 
+const fetchAndDisplayNews = async () => {
+  toggleLoading(true);
+  let url = `https://openapi.programming-hero.com/api/news/category/${categoryIdToShow}`;
 
-// show news by categoryId
-const handleCategoryNews = async (categoryId, isShowAll) => {
-  toggleLoading(true)
-
-  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
+  const res = await fetch(url);
   const data = await res.json();
-  toggleLoading(false)
+  toggleLoading(false);
 
-  // connect with card container
-  const cardContainer = document.getElementById('card-container');
+  const cardContainer = document.getElementById("card-container");
 
-  // connect with show more button
-  const button = document.getElementById('show-all')
+  cardContainer.innerHTML = ""; // Clear existing news
 
-  // making the field empty when another link clicked
-  cardContainer.textContent = "";
-
-  if (data.data.length > 3 && !isShowAll) {
-    data.data = data.data.slice(0, 3)
-    button.classList.remove('hidden');
-  }
-  else {
-    button.classList.add('hidden');
-
+  if (data.data.length > 3 && !isShowingAll) {
+    data.data = data.data.slice(0, 3);
+    ShowBtn.classList.remove("hidden");
+  } else {
+    ShowBtn.classList.add("hidden");
   }
 
-  // fetching the data of news by foreach loop
+  if (sortFlag) {
+    data.data.sort((a, b) => b.total_view - a.total_view);
+    sortFlag = false;
+  }
+  // data.data.sort((a, b) => b.total_view - a.total_view);
+
   data.data.forEach((news) => {
-
-    // Creating a card for showing the news details
-    const div = document.createElement('div');
-    div.innerHTML =
-      `<div class="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img src=${news?.image_url}/>
-                    </figure>
-                    <div class="card-body">
-                        <h2 class="card-title">
-                        ${news.title.slice(0, 20)}
-                            <div class="badge badge-secondary p-5">${news?.rating?.badge}</div>
-                        </h2>
-                        <p>
-                            ${news.details.slice(0, 50)}
-                        </p>
-                        <h3> totoal viws: ${news.total_view ? news.total_view : "no vviews"
-      }</h3>
-                        <div class="card-footer flex justify-between items-center mt-3">
-                            <div class="flex items-center">
-                                <div class="p-3">
-                                    <div class="avatar online">
-                                        <div class="w-14 rounded-full">
-                                            <img
-                                            src=${news.author?.img}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <h6>${news.author?.name}</h6>
-                                    <small>2022-08-24 17:27:34</small>
-                                </div>
-                            </div>
-                            <div class="card-detaild-btn">
-                                <button onclick=handleModal('${news._id}')
-                                    class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-                                    Details
-                                </button>
-                            </div>
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="card w-96 bg-base-100 shadow-xl">
+    <figure>
+        <img src=${news?.image_url}/>
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">
+        ${news.title.slice(0, 20)}
+            <div class="badge badge-secondary p-5">${news?.rating?.badge}</div>
+        </h2>
+        <p>
+            ${news.details.slice(0, 50)}
+        </p>
+        <h3> Total Views: ${news.total_view ? news.total_view : "No Views"}</h3>
+        <div class="card-footer flex justify-between items-center mt-3">
+            <div class="flex items-center">
+                <div class="p-3">
+                    <div class="avatar online">
+                        <div class="w-14 rounded-full">
+                            <img
+                            src=${news.author?.img}
+                            />
                         </div>
                     </div>
                 </div>
-    
-            `;
+                <div>
+                    <h6>${news.author?.name}</h6>
+                    <small>2022-08-24 17:27:34</small>
+                </div>
+            </div>
+            <div class="card-detaild-btn">
+                <button onclick=handleModal('${news._id}')
+                    class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+                    Details
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 
-    // apend the card in card container div 
+`;
     cardContainer.appendChild(div);
-  })
+  });
 
-  // Cheacking if any news missing 
-  if (cardContainer.textContent == "") {
-    cardContainer.textContent = "There is no news. Please find another category!"
+  if (cardContainer.children.length === 0) {
+    cardContainer.textContent =
+      "There is no news. Please find another category!";
   }
-}
+};
 
-// loading spinner for showing that the data is loaded
-const toggleLoading = (isloading) => {
-  const loading = document.getElementById('spinner');
-  if (isloading) {
-    loading.classList.remove('hidden')
+const toggleLoading = (isLoading) => {
+  const loading = document.getElementById("spinner");
+  if (isLoading) {
+    loading.classList.remove("hidden");
+  } else {
+    loading.classList.add("hidden");
   }
-  else {
-    loading.classList.add('hidden')
-  }
-}
+};
 
-// showallnews by clicking the show all button
-const showAllBtn = () => {
-  handleCategories(true)
-}
+const handleCategoryNews = async (categoryId) => {
+  console.log(categoryId);
+  categoryIdToShow = categoryId;
+  isShowingAll = false;
+  fetchAndDisplayNews();
+};
 
-// calling the handleCategories function
-handleCategories()
-handleCategoryNews('04')
+ShowBtn.addEventListener("click", () => {
+  isShowingAll = true;
+  fetchAndDisplayNews();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+handleCategories();
